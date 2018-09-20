ee9a40c7e3a72646c089aa15b5458b2b6f25ac55
jonathanrbarney <jonathanrbarney@gmail.com>
2018-09-19 20:46:38 -0700

    Fix for loading.

--- bid/settings.py
+++ bid/settings.py
@@ -124,4 +124,7 @@ STATIC_ROOT = os.path.dirname(BASE_DIR) + '/public/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.dirname(BASE_DIR) + '/public/media/'
 
+SECURE_SSL_REDIRECT = True
 
+PROJECT_PATH = os.path.abspath(os.path.dirname(__name__))
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
